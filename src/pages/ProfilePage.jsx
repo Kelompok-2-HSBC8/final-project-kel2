b65900b7d34bb244f2b09edb4954fb933aeb9b1a
Ricky Riskiawan <rickyrizkiawan666@gmail.com>
2023-12-08 11:38:57 +0700

    adding feat/profile/follow

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import moment from "moment/moment";
 import ModalEditProfile from "../components/ModalEditProfile";
 
@@ -111,16 +111,20 @@ function ProfilePage() {
             </div>
             <div className="flex flex-row p-3 relative bottom-[6.4rem]">
                 <div className="flex flex-row justify-center items-center mr-4">
-                    <span className="ml-1 font-bold">{0}</span>
-                    <span className="font-[300] mx-2 text-gray-500">
-                        Mengikuti
-                    </span>
+                    <Link to="/profile/following" className="hover:border-b">
+                        <span className="ml-1 font-bold">{0}</span>
+                        <span className="font-[300] mx-2 text-gray-500">
+                            Mengikuti
+                        </span>
+                    </Link>
                 </div>
                 <div className="flex flex-row justify-center items-center mr-4">
-                    <span className="ml-1 font-bold">{0}</span>
-                    <span className="font-[300] mx-2 text-gray-500">
-                        Pengikut
-                    </span>
+                    <Link to="/profile/followers" className="hover:border-b">
+                        <span className="ml-1 font-bold">{0}</span>
+                        <span className="font-[300] mx-2 text-gray-500">
+                            Pengikut
+                        </span>
+                    </Link>
                 </div>
             </div>
             <div className="flex flex-row relative bottom-24 border border-t-0 boder-r-0 border-l-0">
